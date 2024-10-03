96a00031e2bf311e7f24d9a851d6104b5b795217
Tyson Meade <tyson.meade@students.snow.edu>
2024-10-03 12:23:09 -0600

    Oct3

---
+++ InClassCode/Oct3/site.js
@@ -0,0 +1,33 @@
+//let - default
+
+let myName = "0";
+let number = 25;
+let isHungry = true;
+
+let result = myName + number;
+
+//const - constant. Doesn't prevent you from changing it.
+
+const pieVal = 3.14;
+
+//var - don't use in js
+
+//js says that this is undefined. Declared but not assigned.
+let thisVar;
+
+//null is intentional.
+let someOtherThing = null;
+
+console.log(myName);
+console.log(someOtherThing);
+
+//object
+let person = {
+   name: "John",
+   age: 25
+};
+
+//array
+let colors = ["red", "blue", "green"];
+
+console.log(colors[0]);
